ae12b08923baa1fcc51cf1c522c375932aa165e2
Connor Olson <connorolson123@gmail.com>
2021-08-03 23:03:56 -0700

    port crashing glitch fix 6

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const sequelize = require("./config/connection")
 
-// Sets up the Express App
-// =============================================================
+// express
 const app = express();
 const PORT = process.env.PORT || 3000;
 const routes = require('./controllers');
