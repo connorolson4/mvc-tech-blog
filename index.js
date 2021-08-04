ecc48f444360e39c47d21b9cc1e68c71287aa1d0
Connor Olson <connorolson123@gmail.com>
2021-08-03 22:48:24 -0700

    port crashing glitch fix 4

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ const sequelize = require("./config/connection")
 // Sets up the Express App
 // =============================================================
 const app = express();
-const PORT = process.env.PORT || 3000;
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  });
 const routes = require('./controllers');
 
 
